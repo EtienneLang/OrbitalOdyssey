384c6b427d2b6c6d3f9fd117519de68b98ec0f25
etiennelanglois007 <etiennelanglois007@gmail.com>
2024-06-27 12:46:49 -0400

    Ajout de mercure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="app">
     <div ref="rendererContainer" class="renderer-container"></div>
-  </div>
 </template>
 
 <script>
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import * as THREE from 'three';
-import textureSun from './assets/images/chat.png';
+import textureSun from './assets/textures/sun_texture.jpg';
+import textureMercury from './assets/textures/mercury_texture.jpg';
 
 export default {
   name: 'App',
   setup() {
     const rendererContainer = ref(null);
-    let scene, camera, renderer, sun;
+    let scene, camera, renderer, sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto;
 
     const initThreeJS = () => {
       // Scene, Camera, Renderer
@@ -24,23 +23,11 @@ export default {
       renderer.setSize(window.innerWidth, window.innerHeight);
       rendererContainer.value.appendChild(renderer.domElement);
 
-      // Load Sun texture
-      const textureLoader = new THREE.TextureLoader();
-      const sunTexture = textureLoader.load(textureSun, () => {
-        // Create Sun geometry and material after texture is loaded
-        const sunGeometry = new THREE.SphereGeometry(5, 32, 32); // Adjust size as needed
-        const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
-
-        // Create Sun mesh
-        sun = new THREE.Mesh(sunGeometry, sunMaterial);
-
-        // Add Sun to the scene
-        scene.add(sun);
-      });
-
-      // Handle window resize
+      createSunMesh();
+      createMercuryMesh();
       window.addEventListener('resize', onWindowResize, false);
     };
+
 
     const onWindowResize = () => {
       camera.aspect = window.innerWidth / window.innerHeight;
@@ -50,13 +37,40 @@ export default {
 
     const animate = () => {
       requestAnimationFrame(animate);
-
-      if (sun) {
+      if (sun && mercury) {
         // Optional: Rotate the Sun for a better effect
-        sun.rotation.y += 0.005;
+        sun.rotation.y += 0.0005;
+        mercury.rotation.y += 0.01;
       }
 
       renderer.render(scene, camera);
+    };
+
+    const createSunMesh = () => {
+      const textureLoader = new THREE.TextureLoader();
+          const sunTexture = textureLoader.load(textureSun, () => {
+            // Create Sun geometry and material after texture is loaded
+            const sunGeometry = new THREE.SphereGeometry(5, 32, 32); // Adjust size as needed
+            const sunMaterial = new THREE.MeshBasicMaterial({map: sunTexture});
+
+            // Create Sun mesh
+            sun = new THREE.Mesh(sunGeometry, sunMaterial);
+            scene.add(sun);
+          });
+    };
+    const createMercuryMesh = () => {
+      const textureLoader = new THREE.TextureLoader();
+          const mercuryTexture = textureLoader.load(textureMercury, () => {
+            // Create Sun geometry and material after texture is loaded
+            const mercuryGeometry = new THREE.SphereGeometry(1, 32, 32); // Adjust size as needed
+
+            const mercuryMaterial = new THREE.MeshBasicMaterial({map: mercuryTexture});
+
+            // Create Sun mesh
+            mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
+            scene.add(mercury);
+            mercury.position.x = 10;
+          });
     };
 
     onMounted(() => {
